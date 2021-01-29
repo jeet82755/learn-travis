d7f12daa934bce94cc460b9f96672a7369f4a839
Jeet Kaushal Gujarathi <43495235+jeet82755@users.noreply.github.com>
2021-01-29 12:44:58 +0530

    Update Main JS file with comments

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
+  //Node Js mail file 
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n'); // build should pass now!
 }).listen(1337, '127.0.0.1');
